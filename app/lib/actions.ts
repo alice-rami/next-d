b1b1467a7f9fae002fd1c9ab57c3f9370d66217c
Alice R <alicepr@yandex.ru>
2024-07-30 13:30:14 +0300

    start ch 16

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
 	id: z.string(),
@@ -69,25 +71,35 @@ export async function createInvoice(prevState: State, formData: FormData) {
 	redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-	const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+	id: string,
+	prevState: State,
+	formData: FormData
+) {
+	const validatedFields = UpdateInvoice.safeParse({
 		customerId: formData.get('customerId'),
 		amount: formData.get('amount'),
 		status: formData.get('status'),
 	});
 
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: 'Missing Fields. Failed to Update Invoice.',
+		};
+	}
+
+	const { customerId, amount, status } = validatedFields.data;
 	const amountInCents = amount * 100;
 
 	try {
 		await sql`
-			UPDATE invoices
-			SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-			WHERE id = ${id}
-		`;
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
 	} catch (error) {
-		return {
-			message: 'Database Error: Failed to Update Invoice',
-		};
+		return { message: 'Database Error: Failed to Update Invoice.' };
 	}
 
 	revalidatePath('/dashboard/invoices');
@@ -95,7 +107,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-	throw new Error('Failed to Delete Invoice');
+	// throw new Error('Failed to Delete Invoice');
 
 	try {
 		await sql`DELETE FROM invoices WHERE id = ${id}`;
@@ -105,5 +117,24 @@ export async function deleteInvoice(id: string) {
 		return {
 			message: 'Database Error: Failed to Delete Invoice',
 		};
+	}
+}
+
+export async function authenticate(
+	prevState: string | undefined,
+	formData: FormData
+) {
+	try {
+		await signIn('credentials', formData);
+	} catch (error) {
+		if (error instanceof AuthError) {
+			switch (error.type) {
+				case 'CredentialsSignin':
+					return 'Invalid credentials.';
+				default:
+					return 'Something went wrong.';
+			}
+		}
+		throw error;
 	}
 }
